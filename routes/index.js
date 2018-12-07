e2ce44c1f3d5db77c5cf7c47f5f01732d5190b9d
Angelini <lllouis@yahoo.com>
2018-12-07 19:16:25 +0000

    Finished

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,25 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        url: 'http://www.stark-living.com'
+        url: 'http://www.stark-living.com',
+        image_url: 'http://www.stark-living.com/images/fb_garden.jpeg'
     });
 });
 
-router.get('/kindling-splitter', function( req, res, next ) {
-    res.render( 'kindling_splitter', {
-        url: 'http://www.stark-living.com'
-    } )
+router.get('/kindling-splitter', function (req, res, next) {
+    res.render('kindling_splitter', {
+        url: 'http://www.stark-living.com/kindling-spitter',
+        image_url: 'http://www.stark-living.com/images/fb_garden.jpeg'
+    })
 })
 
 
-router.get('/jack-cover', function( req, res, next ) {
-    res.render( 'jack_cover' )
+router.get('/jack-cover', function (req, res, next) {
+    res.render('jack_cover')
 })
 
-router.get('/tick-removal', function( req, res, next ) {
-    res.render( 'tick_removal' )
+router.get('/tick-removal', function (req, res, next) {
+    res.render('tick_removal')
 })
 
 module.exports = router;
