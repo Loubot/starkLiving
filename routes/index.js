3b40a3c76454b387d6fb36e7188d656e36812186
Angelini <Lllouis@yahoo.com>
2018-12-08 16:28:45 +0000

    Add pics and urls

--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,23 @@ router.get('/', function (req, res, next) {
 router.get('/kindling-splitter', function (req, res, next) {
     res.render('kindling_splitter', {
         url: 'http://www.stark-living.com/kindling-spitter',
-        image_url: 'http://www.stark-living.com/images/fb_garden.jpeg'
+        image_url: 'http://www.stark-living.com/images/fb_splitter.jpg'
     })
 })
 
 
 router.get('/jack-cover', function (req, res, next) {
-    res.render('jack_cover')
+    res.render('jack_cover', {
+        url: 'http://www.stark-living.com/kindling-spitter/jack-cover',
+        image_url: 'http://www.stark-living.com/images/fb_jack_cover.jpeg'
+    })
 })
 
 router.get('/tick-removal', function (req, res, next) {
-    res.render('tick_removal')
+    res.render('tick_removal', {
+        url: 'http://www.stark-living.com/kindling-spitter/tick-removal',
+        image_url: 'http://www.stark-living.com/images/fb_tick_removal.jpeg'
+    })
 })
 
 module.exports = router;
